ef57b54f53181a92b19bf33cb5496cd1462f357e
leanderson01 <leandersonnunes.alu.lmb@gmail.com>
2025-04-12 12:50:40 -0300

    fix: Removing changing theme error

--- src/components/change-theme-btn.tsx
+++ src/components/change-theme-btn.tsx
@@ -2,13 +2,23 @@
 
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
 
 function ChangeThemeBtn() {
   const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Só executa após a montagem do componente no cliente
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Evita renderização do valor do tema no servidor
+  const buttonText = mounted ? `Change Theme to (${theme})` : "Change Theme";
 
   return (
     <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      Change Theme to ({theme})
+      {buttonText}
     </Button>
   );
 }
