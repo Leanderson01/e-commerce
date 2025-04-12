9428e7d759f3721261f8b82ddc2f25acad3d0fc4
leanderson01 <leandersonnunes.alu.lmb@gmail.com>
2025-04-11 21:02:43 -0300

    feat: Adding devtools

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -6,7 +6,7 @@ import { createTRPCReact } from "@trpc/react-query";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import { useState } from "react";
 import SuperJSON from "superjson";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type AppRouter } from "~/server/api/root";
 import { createQueryClient } from "./query-client";
 
@@ -66,6 +66,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <api.Provider client={trpcClient} queryClient={queryClient}>
         {props.children}
+        {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
       </api.Provider>
     </QueryClientProvider>
   );
